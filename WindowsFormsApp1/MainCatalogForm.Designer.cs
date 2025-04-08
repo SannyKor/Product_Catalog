ce9b1c9b09cb57757dd528f730d323290330e470
Олександр Олександр <sanny_kor@outlook.com>
2025-04-09 01:20:41 +0300

    добавив пошук по артикулу в списку добавив contextMenuStrip

--- WindowsFormsApp1/MainCatalogForm.Designer.cs
+++ WindowsFormsApp1/MainCatalogForm.Designer.cs
@@ -28,11 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.requestId_field = new System.Windows.Forms.TextBox();
             this.buttonSearch = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemEditUnit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemDelUnit = new System.Windows.Forms.ToolStripMenuItem();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBoxIdSearch = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // requestId_field
@@ -51,19 +62,71 @@
             this.buttonSearch.TabIndex = 8;
             this.buttonSearch.Text = "Пошук";
             this.buttonSearch.UseVisualStyleBackColor = true;
-            this.buttonSearch.Click += new System.EventHandler(this.button4_Click);
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 207);
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1039, 352);
+            this.dataGridView1.Size = new System.Drawing.Size(934, 352);
             this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_2);
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemEditUnit,
+            this.toolStripMenuItemDelUnit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            // 
+            // toolStripMenuItemEditUnit
+            // 
+            this.toolStripMenuItemEditUnit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.toolStripMenuItemEditUnit.Name = "toolStripMenuItemEditUnit";
+            this.toolStripMenuItemEditUnit.Size = new System.Drawing.Size(210, 24);
+            this.toolStripMenuItemEditUnit.Text = "Редагувати";
+            this.toolStripMenuItemEditUnit.Click += new System.EventHandler(this.toolStripMenuItemEditUnit_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(225, 26);
+            this.toolStripMenuItem3.Text = "toolStripMenuItem3";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(225, 26);
+            this.toolStripMenuItem4.Text = "toolStripMenuItem4";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(225, 26);
+            this.toolStripMenuItem5.Text = "toolStripMenuItem5";
+            // 
+            // toolStripMenuItemDelUnit
+            // 
+            this.toolStripMenuItemDelUnit.Name = "toolStripMenuItemDelUnit";
+            this.toolStripMenuItemDelUnit.Size = new System.Drawing.Size(210, 24);
+            this.toolStripMenuItemDelUnit.Text = "Видалити";
+            this.toolStripMenuItemDelUnit.Click += new System.EventHandler(this.toolStripMenuItemDelUnit_Click);
             // 
             // button3
             // 
@@ -75,11 +138,36 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBoxIdSearch
+            // 
+            this.textBoxIdSearch.Location = new System.Drawing.Point(80, 169);
+            this.textBoxIdSearch.Name = "textBoxIdSearch";
+            this.textBoxIdSearch.Size = new System.Drawing.Size(75, 22);
+            this.textBoxIdSearch.TabIndex = 12;
+            this.textBoxIdSearch.TextChanged += new System.EventHandler(this.textBoxIdSearch_TextChanged);
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(12, 172);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(65, 16);
+            this.labelId.TabIndex = 13;
+            this.labelId.Text = "Артикул:";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
             // MainCatalogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1039, 559);
+            this.ClientSize = new System.Drawing.Size(958, 559);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.textBoxIdSearch);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonSearch);
@@ -88,6 +176,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,6 +187,15 @@
         private System.Windows.Forms.Button buttonSearch;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxIdSearch;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemEditUnit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemDelUnit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
     }
 }
 
--- WindowsFormsApp1/MainCatalogForm.cs
+++ WindowsFormsApp1/MainCatalogForm.cs
@@ -14,19 +14,42 @@ namespace WindowsFormsApp1
 {
     public partial class MainCatalogForm : Form
     {
-        private Catalog catalog;
+        protected Catalog catalog;
 
         public MainCatalogForm()
         {
             
             catalog = new Catalog(new StorageFromFile());
             InitializeComponent();
+            SetupDataGridView();
 
-            dataGridView1.DataSource = null;
-            dataGridView1.DataSource = catalog.Units;
+            //dataGridView1.DataSource = null;
+            //dataGridView1.DataSource = catalog.Units;
+            dataGridView1.Rows.Clear();
+            foreach (Unit unit in catalog.Units)
+            {
+                dataGridView1.Rows.Add(unit.Id, unit.Name, unit.Description, unit.Price, unit.Quantity);
+            }
 
 
-        }        
+        }     
+        private void SetupDataGridView()
+        {
+            /*dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "Id";
+            dataGridView1.Columns[1].Name = "Назва";
+            dataGridView1.Columns[2].Name = "Опис";
+            dataGridView1.Columns[3].Name = "Ціна";
+            dataGridView1.Columns[4].Name = "Кількість";*/
+
+            dataGridView1.AutoGenerateColumns = false;
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Артикул", DataPropertyName = "Id", Name = "Id", Width = 50 });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Назва", DataPropertyName = "Name", Name = "Name", Width = 150 });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Опис", DataPropertyName = "Description", Name = "Description", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Ціна", DataPropertyName = "Price", Name = "Price", Width = 75 });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Кількість", DataPropertyName = "Quantity", Name = "Quantity", Width = 75 });
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -53,7 +76,7 @@ namespace WindowsFormsApp1
            
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void buttonSearch_Click(object sender, EventArgs e)
         {
             string findId = requestId_field.Text;
             //Unit unit = catalog.GetUnitById(Convert.ToInt32(findId));
@@ -92,7 +115,58 @@ namespace WindowsFormsApp1
                     addUnitForm.unitPrice,
                     addUnitForm.unitQuantity
                     );
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = catalog.Units;
             }
+        }
+
+        private void textBoxIdSearch_TextChanged(object sender, EventArgs e)
+        {
+            string input = textBoxIdSearch.Text.Trim();
+            dataGridView1.ClearSelection();
+            if (int.TryParse(input, out int idToFind))
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Cells["Id"].Value != null && Convert.ToInt32(row.Cells["Id"].Value) == idToFind)
+                    {
+                        row.Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = row.Index;
+                        return;
+                    }
+                }
+               dataGridView1.ClearSelection();
+            }
+            else
+            {
+                dataGridView1.ClearSelection();
+            }
+
+        }
+
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                var hit = dataGridView1.HitTest(e.X, e.Y);
+                if (hit.RowIndex >= 0)
+                {
+                    dataGridView1.ClearSelection();
+                    dataGridView1.Rows[hit.RowIndex].Selected = true;
+                    dataGridView1.CurrentCell = dataGridView1.Rows[hit.RowIndex].Cells[0];
+                    contextMenuStrip1.Show(dataGridView1, e.Location);
+                }
+            }
+        }
+
+        private void toolStripMenuItemEditUnit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItemDelUnit_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
